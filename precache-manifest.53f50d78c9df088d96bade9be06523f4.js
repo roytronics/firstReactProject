a7cf7cc16512496201f382f3a58d19177679084c
Roy Garcia <56310492+roytronics@users.noreply.github.com>
2019-12-12 22:11:05 -0800

    Updates

---
+++ precache-manifest.53f50d78c9df088d96bade9be06523f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea8335e1657dd81553ff15f56e30e19",
+    "url": "/firstReactProject/index.html"
+  },
+  {
+    "revision": "402a907179970a36b470",
+    "url": "/firstReactProject/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "1f70a34c574307f818f4",
+    "url": "/firstReactProject/static/css/main.52bfb405.chunk.css"
+  },
+  {
+    "revision": "402a907179970a36b470",
+    "url": "/firstReactProject/static/js/2.ee428d40.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/firstReactProject/static/js/2.ee428d40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f70a34c574307f818f4",
+    "url": "/firstReactProject/static/js/main.16716aea.chunk.js"
+  },
+  {
+    "revision": "23c41913b21420d1c6af",
+    "url": "/firstReactProject/static/js/runtime-main.cd493b94.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/firstReactProject/static/media/SEGA.12a3db8b.woff"
+  }
+]);
