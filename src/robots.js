f46e1baea65ad4585742b4604b4e915ff6ca998f
Roy Garcia <56310492+roytronics@users.noreply.github.com>
2019-12-12 22:06:44 -0800

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 10,
+      name: 'Roy Garcia',
+      username: 'Roy',
+      email: 'garcia@roy.biz'
+    },
+    {
+      id: 20,
+      name: 'Julia Gab',
+      username: 'Julia',
+      email: 'Julia@romania.io'
+    },
+    {
+      id: 30,
+      name: 'Tom Brady',
+      username: 'Tom',
+      email: 'Tom@NE.net'
+    },
+    {
+      id: 40,
+      name: 'James White',
+      username: 'James',
+      email: 'James.White@patriots.org'
+    },
+    {
+      id: 50,
+      name: 'Bruce Wayne',
+      username: 'Bruce',
+      email: 'Bruce_Wayne@batman.ca'
+    },
+    {
+      id: 60,
+      name: 'Tony Stark',
+      username: 'Stark',
+      email: 'stark@avengers.info'
+    },
+    {
+      id: 70,
+      name: 'Mark Rober',
+      username: 'Mark.Rober',
+      email: 'rober@rover.biz'
+    },
+    {
+      id: 80,
+      name: 'Jeff Seid',
+      username: 'Jeff',
+      email: 'Seid@gains.me'
+    },
+    {
+      id: 90,
+      name: 'Skip Bayless',
+      username: 'Skip',
+      email: 'Skip_Bayless@fox.io'
+    },
+    {
+      id: 100,
+      name: 'LeBron James',
+      username: 'James.LeBron',
+      email: 'LJ.23@lakers.biz'
+    }
+  ];
+  
